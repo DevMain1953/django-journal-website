b114627786c7abcc2bb98e01b1bf1b5775c1ae83
DevMain1953 <alexanderbrenenburgh@gmail.com>
2023-04-20 17:40:13 +0300

    added some tests for db

---
+++ website/journal_website/tests/InitialStringsTest.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from ..repositories import StringRepository
+from ..models import String, ScientificPublication, StaticPage, Volume
+
+
+string_repository = StringRepository.StringRepository()
+
+
+class InitialStringsTest(TestCase):
+    def setUp(self):
+        string_repository.add_new_string("Научное издание", "Scientific publication")
+        string_repository.add_new_string("Научная специальность", "Scientific specialty")
+        string_repository.add_new_string("Статическая страница", "Static page")
+        string_repository.add_new_string("Раздел 1", "Volume 1")
+        string_repository.add_new_string("Категория 1", "Category 1")
+    
+
+    def test_newly_created_strings(self):
+        name_of_scientific_publication = String.objects.get(pk=1)
+        name_of_scientific_specialty = String.objects.get(pk=2)
+        name_of_static_page = String.objects.get(pk=3)
+        name_of_volume = String.objects.get(pk=4)
+        name_of_category = String.objects.get(pk=5)
+
+        self.assertEqual(name_of_scientific_publication.russian, "Научное издание")
+        self.assertEqual(name_of_scientific_publication.english, "Scientific publication")
+
+        self.assertEqual(name_of_scientific_specialty.russian, "Научная специальность")
+        self.assertEqual(name_of_scientific_specialty.english, "Scientific specialty")
+
+        self.assertEqual(name_of_static_page.russian, "Статическая страница")
+        self.assertEqual(name_of_static_page.english, "Static page")
+
+        self.assertEqual(name_of_volume.russian, "Раздел 1")
+        self.assertEqual(name_of_volume.english, "Volume 1")
+
+        self.assertEqual(name_of_category.russian, "Категория 1")
+        self.assertEqual(name_of_category.english, "Category 1")
---
+++ website/journal_website/tests/NewCategoryCreatingTest.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from ..repositories import StringRepository
+from ..models import String, ScientificPublication, StaticPage, Category, ScientificSpecialty
+
+
+string_repository = StringRepository.StringRepository()
+
+
+class NewCategoryCreatingTest(TestCase):
+    def setUp(self):
+        new_name_of_scientific_publication = string_repository.add_new_string("Научное издание", "Scientific publication")
+        new_name_of_scientific_specialty = string_repository.add_new_string("Научная специальность", "Scientific specialty")
+        new_name_of_static_page = string_repository.add_new_string("Статическая страница", "Static page")
+        new_name_of_category = string_repository.add_new_string("Первая категория", "First category")
+        new_static_page = StaticPage.objects.create(name=new_name_of_static_page, relative_url_address="test_url")
+        new_scientific_publication = ScientificPublication.objects.create(full_name=new_name_of_scientific_publication, short_name=new_name_of_scientific_publication, contacts=new_static_page,
+                                             main_editor=new_static_page, legal_information=new_static_page, contract_information=new_static_page, science_branches=new_static_page,
+                                             editorial_team=new_static_page)
+        new_scientific_specialty = ScientificSpecialty.objects.create(name=new_name_of_scientific_specialty, science_branches=new_static_page)
+        Category.objects.create(name=new_name_of_category, scientific_specialty=new_scientific_specialty, scientific_publication=new_scientific_publication)
+
+
+    def test_newly_created_category(self):
+        self.assertEqual(Category.objects.get(pk=1).name.russian, "Первая категория")
+        self.assertEqual(Category.objects.get(pk=1).name.english, "First category")
