2d18d0a2188c1d12b31a5c19b009d8ba6bb076a5
DevMain1953 <alexanderbrenenburgh@gmail.com>
2023-05-05 00:26:01 +0300

    changed name of result dict in methods of article manager

--- website/journal_website/managers/ArticleManager.py
+++ website/journal_website/managers/ArticleManager.py
@@ -9,7 +9,7 @@ class ArticleManager:
     
 
     def get_name_description_and_authors_of_article_in_russian_from_file(self, file_name: str) -> Dict[str, str]:
-        base_information = {}
+        result = {}
         number_of_paragraph = 0
         path_to_file = settings.MEDIA_ROOT + file_name
         document = self.__docx_document_parser(path_to_file)
@@ -17,20 +17,20 @@ class ArticleManager:
             if paragraph.style.name == "Main Title":
                 number_of_paragraph += 1
                 if number_of_paragraph == 2:
-                    base_information["name"] = paragraph.text
+                    result["name"] = paragraph.text
                 if number_of_paragraph == 3:
-                    base_information["short_description"] = paragraph.text
+                    result["short_description"] = paragraph.text
         number_of_paragraph = 0
         for paragraph in document.paragraphs:
             if paragraph.style.name == "University Name":
                 number_of_paragraph += 1
                 if number_of_paragraph == 1:
-                    base_information["authors"] = paragraph.text
-        return base_information
+                    result["authors"] = paragraph.text
+        return result
     
 
     def get_name_description_and_authors_of_article_in_english_from_file(self, file_name: str) -> Dict[str, str]:
-        base_information = {}
+        result = {}
         number_of_paragraph = 0
         path_to_file = settings.MEDIA_ROOT + file_name
         document = self.__docx_document_parser(path_to_file)
@@ -38,13 +38,13 @@ class ArticleManager:
             if paragraph.style.name == "Main Title":
                 number_of_paragraph += 1
                 if number_of_paragraph == 4:
-                    base_information["name"] = paragraph.text
+                    result["name"] = paragraph.text
                 if number_of_paragraph == 5:
-                    base_information["short_description"] = paragraph.text
+                    result["short_description"] = paragraph.text
         number_of_paragraph = 0
         for paragraph in document.paragraphs:
             if paragraph.style.name == "University Name":
                 number_of_paragraph += 1
                 if number_of_paragraph == 3:
-                    base_information["authors"] = paragraph.text
-        return base_information
+                    result["authors"] = paragraph.text
+        return result
